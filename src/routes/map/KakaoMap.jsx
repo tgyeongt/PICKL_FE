cadaa64c7a15735c47765d5891f5e2804f2c9868
박태근 <taegeun1234@naver.com>
2025-08-05 22:07:08 +0900

    ✨ Feat: Map - 현재 위치로 이동 버튼 추가

--- src/routes/map/KakaoMap.jsx
+++ src/routes/map/KakaoMap.jsx
@@ -1,7 +1,10 @@
-import { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { useEffect, useRef, useState } from "react";
+import CurrentLocationImg from "@icon/vector.png";
 
 export default function KakaoMap() {
   const mapRef = useRef(null);
+  const [mapInstance, setMapInstance] = useState(null);
 
   const loadScript = () => {
     if (document.querySelector("script[src*='dapi.kakao.com']")) return Promise.resolve();
@@ -25,7 +28,8 @@ export default function KakaoMap() {
           center: new window.kakao.maps.LatLng(latitude, longitude),
           level: 3,
         };
-        new window.kakao.maps.Map(container, options);
+        const map = new window.kakao.maps.Map(container, options);
+        setMapInstance(map);
       });
     });
   };
@@ -34,13 +38,56 @@ export default function KakaoMap() {
     loadScript().then(createMap);
   }, []);
 
+  const handleMoveToCurrentLocation = () => {
+    if (!mapInstance) return;
+
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      const { latitude, longitude } = coords;
+      const moveLatLng = new window.kakao.maps.LatLng(latitude, longitude);
+      mapInstance.panTo(moveLatLng);
+    });
+  };
+
   return (
-    <div
-      ref={mapRef}
-      style={{
-        width: "100%",
-        height: "100%",
-      }}
-    />
+    <KakaoMapWrapper>
+      <KakaoMapBox ref={mapRef} />
+      <CurrentLocationButton onClick={handleMoveToCurrentLocation}>
+        <CurrentLocationIcon src={CurrentLocationImg} alt="현재 위치" />
+      </CurrentLocationButton>
+    </KakaoMapWrapper>
   );
 }
+
+const KakaoMapWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const KakaoMapBox = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const CurrentLocationButton = styled.button`
+  position: absolute;
+  bottom: 80px;
+  left: 20px;
+  z-index: 10;
+  background-color: #ffffff;
+  border: 1px solid #ccc;
+  border-radius: 48px;
+  width: 46px;
+  height: 46px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CurrentLocationIcon = styled.img`
+  width: 29.04px;
+  height: 29.04px;
+`;
