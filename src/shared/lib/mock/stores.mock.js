ef7212c7f35208e368bba3e6ee1ea571bf7a16ad
박태근 <taegeun1234@naver.com>
2025-08-06 13:10:27 +0900

    ✨ Feat: stores.mock.js 추가 - 백엔드 없이 테스트하기 위해

---
+++ src/shared/lib/mock/stores.mock.js
@@ -0,0 +1,16 @@
+export const mockStoresData = [
+  {
+    id: 1,
+    name: "길음시장",
+    type: "market",
+    latitude: 37.618456,
+    longitude: 127.027621,
+  },
+  {
+    id: 2,
+    name: "이마트 미아점",
+    type: "mart",
+    latitude: 37.619281,
+    longitude: 127.030392,
+  },
+];
