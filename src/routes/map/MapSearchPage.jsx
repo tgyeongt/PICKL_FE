49afc51136bfa98e6164ee19a422e13cfc5ba934
박태근 <taegeun1234@naver.com>
2025-08-07 18:14:42 +0900

    🎨 Design: MapSearchPage - CSS 수정

--- src/routes/map/MapSearchPage.jsx
+++ src/routes/map/MapSearchPage.jsx
@@ -14,43 +14,111 @@ export default function MapSearchPage() {
 
   return (
     <MapSearchWrapper>
-      <SearchBar>
-        <BackButton onClick={() => navigate(-1)}>
-          <BackIcon src={backIcon} alt="뒤로가기" />
-        </BackButton>
-        <SearchInput
-          value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
-          placeholder="시장 또는 마트를 검색해보세요"
-        />
-        <ClearButton>
-          <ClearIcon src={clearIcon} alt="지우기" onClick={() => setKeyword("")} />
-        </ClearButton>
-      </SearchBar>
-      <LocationBox onClick={() => navigate("/map/edit-location")}>
-        <AddressTextWrapper>
-          <AddressText>{selectedAddress.roadAddress || selectedAddress.jibunAddress}</AddressText>
-          <DropdownImg src={DropdownIcon} alt="드롭다운" />
-        </AddressTextWrapper>
-      </LocationBox>
-      <LocationBox></LocationBox>
+      <BackButton onClick={() => navigate(-1)}>
+        <BackIcon src={backIcon} alt="뒤로가기" />
+      </BackButton>
+      <MapSearchBox>
+        <SearchBar>
+          <SearchInput
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+            placeholder="시장 or 마트를 검색해보세요"
+          />
+          {keyword && (
+            <ClearButton>
+              <ClearIcon src={clearIcon} alt="지우기" onClick={() => setKeyword("")} />
+            </ClearButton>
+          )}
+        </SearchBar>
+        <LocationBox onClick={() => navigate("/map/edit-location")}>
+          <AddressTextWrapper>
+            <AddressText>{selectedAddress.roadAddress || selectedAddress.jibunAddress}</AddressText>
+            <DropdownImg src={DropdownIcon} alt="드롭다운" />
+          </AddressTextWrapper>
+        </LocationBox>
+      </MapSearchBox>
     </MapSearchWrapper>
   );
 }
 
-const MapSearchWrapper = styled.div``;
+const MapSearchWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  min-height: 100vh;
+  width: 100%;
+  overflow-y: auto;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 13px;
+  padding-bottom: 0;
+`;
 
-const SearchBar = styled.div``;
+const BackButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 5px 0;
+  display: flex;
+  align-items: center;
+  width: 35px;
+  height: 35px;
+`;
 
-const BackButton = styled.button``;
+const BackIcon = styled.img`
+  width: 24px;
+  height: 24px;
+`;
 
-const BackIcon = styled.img``;
+const MapSearchBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
 
-const SearchInput = styled.input``;
+const SearchBar = styled.div`
+  width: 100%;
+  height: 35px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  padding: 5px 15px;
+  border-radius: 15px;
+  background: #eaeaed;
+`;
 
-const ClearButton = styled.button``;
+const SearchInput = styled.input`
+  flex: 1;
+  border: none;
+  outline: none;
+  background: transparent;
+  color: #333;
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin-left: 11px;
 
-const ClearIcon = styled.img``;
+  &::placeholder {
+    color: #adadaf;
+  }
+`;
+
+const ClearButton = styled.button`
+  background: none;
+  border: none;
+  position: absolute;
+  right: 12px;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  padding: 0;
+`;
+
+const ClearIcon = styled.img`
+  width: 18px;
+  height: 18px;
+`;
 
 const LocationBox = styled.div`
   cursor: pointer;
