9f0bface2d68eb3bbce324bd8f8ef0e92a7cfb35
박태근 <taegeun1234@naver.com>
2025-08-11 11:29:07 +0900

    ✨ Feat: MyTopBar 구현

--- src/routes/my/MyTopBar.jsx
+++ src/routes/my/MyTopBar.jsx
@@ -1,3 +1,33 @@
+import styled from "styled-components";
+
 export default function MyTopBar() {
-  return <p>MyTopBar</p>;
+  return (
+    <MyTopBarWrapper>
+      <TopText>마이페이지</TopText>
+    </MyTopBarWrapper>
+  );
 }
+
+const MyTopBarWrapper = styled.div`
+  max-width: 390px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fbfbfb;
+  position: sticky;
+  top: 0;
+  z-index: 100;
+  padding-top: calc(env(safe-area-inset-top, 0px) + 14px);
+  padding-bottom: 43px;
+`;
+
+const TopText = styled.p`
+  color: #1c1b1a;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px;
+`;
