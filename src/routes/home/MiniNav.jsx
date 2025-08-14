b447862783f26ddd247ba255f8c27e00098c2502
tgyeongt <tgyeongt@gmail.com>
2025-08-14 17:16:24 +0900

    Fix: 탭 이동 모션 추가

--- src/routes/home/MiniNav.jsx
+++ src/routes/home/MiniNav.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const tabs = ["알뜰소비", "제철식재료", "시세 평균"];
 
@@ -6,9 +7,15 @@ export default function MiniNav({ activeTab, setActiveTab }) {
   return (
     <NavBar>
       {tabs.map((tab) => (
-        <TabButton key={tab} onClick={() => setActiveTab(tab)} $active={activeTab === tab}>
-          {tab}
-        </TabButton>
+        <TabWrapper key={tab} onClick={() => setActiveTab(tab)}>
+          <TabButton $active={activeTab === tab}>{tab}</TabButton>
+          {activeTab === tab && (
+            <ActiveBar
+              layoutId="activeTab"
+              transition={{ type: "spring", stiffness: 400, damping: 50 }}
+            />
+          )}
+        </TabWrapper>
       ))}
     </NavBar>
   );
@@ -19,17 +26,33 @@ const NavBar = styled.div`
   justify-content: space-around;
   padding: 10px 0 0 0;
   background-color: #fbfbfb;
+  position: relative;
 `;
 
-const TabButton = styled.button`
+const TabWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+`;
+
+const TabButton = styled.div`
   width: 120px;
-  height: 25px;
+  height: 40px;
   display: flex;
   justify-content: center;
+  align-items: center;
   font-size: 14px;
   font-weight: ${(props) => (props.$active ? "700" : "500")};
   color: ${(props) => (props.$active ? "#1C1B1A" : "#787885")};
-  border-bottom: ${(props) => (props.$active ? "2px solid #1C1B1A" : "none")};
-  padding: 5px 10px;
-  cursor: pointer;
+`;
+
+const ActiveBar = styled(motion.div)`
+  position: absolute;
+  bottom: 0;
+  height: 2px;
+  width: 100%;
+  background-color: #1c1b1a;
+  border-radius: 1px;
 `;
